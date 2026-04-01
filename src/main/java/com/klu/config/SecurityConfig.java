03de2d0849a778db7e94c383df46dd49756a97c0
MARINGANTI ADITHYA <2400030243@kluniversity.in>
2026-04-01 10:27:30 +0530

    Modify CORS allowed origins
    Updated allowed origins in CORS configuration.

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "https://ecommerce-frontend-production-046e.up.railway.app"
+            "https://ecommerce-frontend1-production-3869.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
